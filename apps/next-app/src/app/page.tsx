4d30361872d1e60a4db6f008fb73c365dffe6cd8
Danil Vodopyanov <danilvodopyanov2nr@gmail.com>
2024-07-18 22:23:53 +0300

    :wrench: update configs

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@repo/ui/button';
 
 export default function Home() {
+  console.log('2');
   return (
     <main className='h-screen bg-cyan-700'>
       <Button className='w-32 h-32' appName='das'>
